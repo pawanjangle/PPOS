47a1707a297fbbb1ec7414b1b95da3aa9e188c93
pawan Jangle <pawanjangle1000@gmail.com>
2024-10-04 14:16:14 +0530

    change frontend directory name

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 // const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 }
 app.use("/api/product/", product);
 
