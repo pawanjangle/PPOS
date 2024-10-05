c7f2667b4610b7da324a198e05fbccc03949f1db
pawan Jangle <pawanjangle1000@gmail.com>
2024-10-05 12:08:03 +0530

    undo changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-to-print']
-    },
-  },
+  plugins: [react()]
+  // ,
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-to-print']
+  //   },
+  // },
 })
