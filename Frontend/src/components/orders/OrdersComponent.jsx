40e96863ea0421951b3b11fc064471dbf715753d
pawanjangle <pawanjangle1000@gmail.com>
2024-10-20 07:28:48 +0530

    changed spelling of file

--- Frontend/src/components/orders/OrdersComponent.jsx
+++ Frontend/src/components/orders/OrdersComponent.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import './OrdersComponent.css'
+import './OrdersComponent.css';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { getOrders, callDeleteOrder } from '../../service/Service';
